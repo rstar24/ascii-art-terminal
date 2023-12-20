192a0d1bcd9278d376b37f6dc180dd7c5162fa0d
Rishabh Rathore <rishabhrathod20@gmail.com>
2023-12-20 14:05:08 +0530

    Added figlet fonts

--- asci_art.sh
+++ asci_art.sh
@@ -1,23 +1,29 @@
 #!/usr/bin/bash
 
-# This is start
 # Take the user input 
-
 echo "Write a string : " | lolcat;
-
 read a ;
-
 clear ;
-
 echo "Enjoy " | lolcat;
+#figlet -d ~/projects/figlet-fonts/ -f Cybermedium.flf $a | pv -qL 800 | lolcat 
+#clear;
+#figlet -f slant.flf $a | pv -qL 800  
+#clear;
+#toilet -f slant.flf --gay $a | pv -qL 800  
+#
 
-figlet -f banner.flf $a | pv -qL 800 | lolcat 
+# Using all the figlet-fonts available
 
-# clear;
+#./figlet-fonts/*.flf 
 
-figlet -f slant.flf $a | pv -qL 800  
+for fonts in ./figlet-fonts/*.flf
+do
+    #figlet -d ./figlet-fonts/ -f "$fonts" -t 80 $a | pv -qL 1000 | lolcat 
+    toilet -d ./figlet-fonts/ -f "$fonts" --gay -t  $a | pv -qL 1000 
+    echo ""
+done 
 
-echo "Intialized" | lolcat
+echo "Done" | lolcat
 
 
 
